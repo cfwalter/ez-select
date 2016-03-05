dc47fbeca01b79bfda7ddbf1bf4556b4d7106728
Christian Walter <walter@lightningkite.com>
2016-03-04 20:11:14 -0700

    select target field from storage

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ var dirty = false;
 
 document.onreadystatechange = function () {
     if (document.readyState == "interactive" || document.readyState == "complete") {
+        chrome.storage.local.get(window.location.href, function(data) {
+            var targetField = document.getElementById(data[window.location.href]);
+            if (!targetField) {
+                var query = 'input[name=' + data[window.location.href] + ']';
+                targetField = document.querySelector(query);
+            }
+            targetField.select();
+            targetField.focus();
+        });
+
         var inputs = document.querySelectorAll('input');
         for (var i = 0; i < inputs.length; ++i) {
             inputs[i].onclick = function(event) {
