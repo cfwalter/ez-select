c7386ccc494c00c5cdc395d4cd5cbb0b8e5cc171
Christian Walter <walter@lightningkite.com>
2016-03-04 20:03:27 -0700

    save target field to storage

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ document.onreadystatechange = function () {
         var inputs = document.querySelectorAll('input');
         for (var i = 0; i < inputs.length; ++i) {
             inputs[i].onclick = function(event) {
+                if (dirty) {
+                    return;
+                }
                 dirty = true;
-                event.stopPropagation();
+                obj = {}
+                obj[window.location.href] = event.target.id ? event.target.id : event.target.name;
+                chrome.storage.local.set(obj, function(){});
             }
         }
     }
